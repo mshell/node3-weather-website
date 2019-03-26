b067b4357e109be9bc4ac3425fb162675d975251
Martin Shell <mshell@alum.mit.edu>
2019-03-26 16:08:15 -0400

    Setup app for heroku

--- src/app.js
+++ src/app.js
@@ -12,6 +12,7 @@ const viewsPath = path.join(__dirname, '../templates/views')
 const partialsPath = path.join(__dirname, '../templates/partials')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 //set up handlebars engine and view path
 app.set('view engine', 'hbs')
@@ -122,8 +123,8 @@ app.get('*', (req, res) => {
 // })
 
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000')
+app.listen(port, () => {
+    console.log('Server is up on port '+ port)
 })
 
 
